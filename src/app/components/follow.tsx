c01de163005cdec6ac1ba0b6c56a4b68757a656e
mohamedelhefenawy <melhefnawy718@gmail.com>
2025-02-01 06:53:43 +0200

    gadgn

--- src/app/components/follow.tsx
+++ src/app/components/follow.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useMediaQuery } from "usehooks-ts";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useMediaQuery } from 'react-responsive';
 
 interface props{
     children : React.ReactNode
 }
 
 export default function Followonmobile({children}:props) {
-  const isMobile = useMediaQuery("(max-width: 768px)"); // Mobile breakpoint
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' }); // Mobile breakpoint
   const router = useRouter();
 
   useEffect(() => {
